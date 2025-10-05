e69165aae1bb8cf1fa92ba475af8216674167e7f
pc <niteshroyalgaming12@gmail.com>
2025-10-05 21:49:44 +0530

    complete of version 1 app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack>
+  return <>
+  <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
+  <Stack>
     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
-  </Stack>;
+  </Stack>
+  </>
 }
